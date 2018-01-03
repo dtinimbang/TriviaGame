b0987332459f930969724cdd2a6f87b3caf50028
don <don.tinimbang@gmail.com>
2018-01-02 23:37:07 -0600

    creating question arrays

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    var triviaGame = [
+         {
+            q1: "Now this is a story...."
+            a: "Fresh Prince"
+            b: "punky Brewster"
+            c: "smiley"
+            d: "simpsons"
+        },
+
+        {
+            q1: "Now this is a story...."
+            a: "Fresh Prince"
+            b: "punky Brewster"
+            c: "smiley"
+            d: "simpsons"
+        },
+
+        {
+            q1: "Now this is a story...."
+            a: "Fresh Prince"
+            b: "punky Brewster"
+            c: "smiley"
+            d: "simpsons"
+        },
+
+        {
+            q1: "Now this is a story...."
+            a: "Fresh Prince"
+            b: "punky Brewster"
+            c: "smiley"
+            d: "simpsons"
+        },
+
+        {
+            q1: "Now this is a story...."
+            a: "Fresh Prince"
+            b: "punky Brewster"
+            c: "smiley"
+            d: "simpsons"
+        },
+    ]
+
+
+
+
+});
+
